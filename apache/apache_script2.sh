78a478dd64e1c56851bd07f3544333caa6fb0b56
luca <luca.fusaro1996@gmail.com>
2023-08-17 21:04:12 -0300

    linux version

--- apache/apache_script2.sh
+++ apache/apache_script2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-yum update -y
-yum install -y httpd.x86_64
-systemctl start httpd.service
-systemctl enable httpd.service
-echo “Hello World from $(hostname -f)” > /var/www/html/index.html
+sudo yum install httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo echo "Hello! New project completed, week 6 here we go!" > /var/www/html/index.html
