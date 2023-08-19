04fdb3086565a42759416d4b7985ef3a6e288a06
luca <luca.fusaro1996@gmail.com>
2023-08-19 19:55:33 -0300

    scripting changes

---
+++ apache/apache_script.txt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Download packages
+sudo yum update
+
+#Install Apache HTTP Web Server
+sudo yum install httpd
+
+#Enable & Start Apache web server
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl status httpd
+
+#Install & Enable Firewall allow web traffic Port 80 & 443
+sudo yum install firewalld
+sudo systemctl enable firewalld
+sudo systemctl status firewalld
+
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --permanent --add-service=https
+
+#Reload Firewall
+sudo firewall-cmd --reload
+
+#Get Public IP Address
+echo “Public IP address:” ‘curl -4 icanhazip.com’
+
+
+
